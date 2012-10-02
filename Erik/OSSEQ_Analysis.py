a283d4dbaa47f3ed743aee494151bcd39e939406
Erik Hopmans <hopmans@stanford.edu>
2012-10-01 21:32:31 -0700

    Introduced on off target + percentage ontarget numbers
    included workaround in case total reads is zero, due to error in
    percentage calculation when dividing by zero

--- Erik/OSSEQ_Analysis.py
+++ Erik/OSSEQ_Analysis.py
@@ -14,8 +14,6 @@ import pybedtools
 
 
 ##### INPUTS AND OUTPUTS #####
-
-##TODO: thing into loop for taking 1 line at a time to process
 ##TODO: make indicator for for where it is in the process.
 ##TODO: incorporate scriptinformation.py for outputlogfile of analysis
 ##TODO: All' 'NMs' 'NMs_oligoC' 'NMs_oligoD' 'Align' 'Align_on_target' 'Align_off_target'
@@ -28,77 +26,71 @@ for row in f:
     fileBed450 = row[3]
     fileBed1500 = row[4]
     bam = pathBam+fileBam
-    bed450 = pathBed+fileBed450
-    bed1500 = pathBed+fileBed1500
     bedFile450 = pathBed+fileBed450
     bedFile1500 = pathBed+fileBed1500
 
-    #Check if input files are correct
 #Check if input files are correct
     if bam[-4:] != ".bam":
-        print "ERROR! The input file is expected to be in the bam file format"
-    if bed450[-4:] != ".bed":
-        print "ERROR! The bed450 file is expected to be in the bed file format"
-    if bed1500[-4:] != ".bed":
-        print "ERROR! The bed1500 file is expected to be in the bed file format"
-
-
-
         print "ERROR in load file! The bam file is expected to be in the bam file format"
     if bedFile450[-4:] != ".bed":
         print "ERROR in load file! The bed450 file is expected to be in the bed file format"
     if bedFile1500[-4:] != ".bed":
         print "ERROR in load file! The bed1500 file is expected to be in the bed file format"
 
-samfile = pysam.Samfile(bam, 'rb')
-bedtool = pybedtools.BedTool(bam)
+# Open bam files and bed files for processing
+    samfile = pysam.Samfile(bam, 'rb')
+    bedtool = pybedtools.BedTool(bam)
+    bed450 = pybedtools.BedTool(bedFile450)
+    bed1500 = pybedtools.BedTool(bedFile1500)
 
-
-bed450 = pybedtools.BedTool('/mnt/cluster2-analysis/Data/Reference_and_Resources/Bed_files/OS0007_JamesBond_450cov.st.collapsed.bed')
-bed1500 = pybedtools.BedTool('/mnt/cluster2-analysis/Data/Reference_and_Resources/Bed_files/OS0007_JamesBond_1500cov.st.collapsed.bed')
-
-#/mnt/cluster2-analysis/Data/Run_Directories/20120309_SG1_0121/Analysis/Alignment/bam/20120309_SG1_0121.c1_p1.st.bam
 
 ##### DEFINE FUNCTIONS #####
-totalReads = 0
-totalAligningReads = 0
+    totalReads = 0
+    totalAligningReads = 0
 
 
 
 ##### SCRIPT #####
 
-#total nr of reads
-samfileTotalReads = samfile.mapped + samfile.unmapped
+#total nr of reads (if zero, an exception is required for calculating the percAlignment to prevent error)
+    samfileTotalReads = samfile.mapped + samfile.unmapped
+    if samfileTotalReads > 0:
+        percAlignment =  ((1.0*samfile.mapped)/samfileTotalReads)*100
+    else:
+        percAlignment = "NA"
 
-print "Total nr of mapped Reads:", samfile.mapped
-print "Total nr of unmapped Reads:", samfile.unmapped
-print "Total nr of Reads:", samfileTotalReads
 
-percAlignment =  ((1.0*samfile.mapped)/samfileTotalReads)*100
 
-print "Percentage Alignment:", percAlignment,"%"
+    print fileBam
+    print "Total nr of mapped Reads:", samfile.mapped
+    print "Total nr of unmapped Reads:", samfile.unmapped
+    print "Total nr of Reads:", samfileTotalReads
+    
+    
+
+    print "Percentage Alignment:", percAlignment,"%"
 
 #################################################################################################################################################
-#On target (within 450bp of capture probes)
-bam_bed450 = bedtool.intersect(bed450).count()
-
-print "Nr of reads on target", bam_bed450
-
-onTargetPerc450 = (1.0*bam_bed450)/totalAligningReads*100
-
-print "Percentage On target:", onTargetPerc450,"%"
-
-#Off target (outside 1500bp of capture probes)
-
-bam_bed1500 = bedtool.intersect(bed1500).count
-offTargetPerc1500 = (1.0*(totalAligningReads-bam_bed1500))/totalAligningReads*100
-
-print "Percentage Off target:", offTargetPerc1500,"%"
-
-#I tried to first do an intersectbed on the bed1500, followed by an intersectbed on that subset with bed450 to increase efficiency, however I get an error that seems to be a bedtools bug
-
-#Qscores and Q score drop? #TODO
-
+##On target (within 450bp of capture probes)
+#bam_bed450 = bedtool.intersect(bed450).count()
+#
+#print "Nr of reads on target", bam_bed450
+#
+#onTargetPerc450 = (1.0*bam_bed450)/totalAligningReads*100
+#
+#print "Percentage On target:", onTargetPerc450,"%"
+#
+##Off target (outside 1500bp of capture probes)
+#
+#bam_bed1500 = bedtool.intersect(bed1500).count
+#offTargetPerc1500 = (1.0*(totalAligningReads-bam_bed1500))/totalAligningReads*100
+#
+#print "Percentage Off target:", offTargetPerc1500,"%"
+#
+##I tried to first do an intersectbed on the bed1500, followed by an intersectbed on that subset with bed450 to increase efficiency, however I get an error that seems to be a bedtools bug
+#
+##Qscores and Q score drop? #TODO
+#
 
 
 

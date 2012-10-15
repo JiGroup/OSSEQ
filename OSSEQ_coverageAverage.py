65be1cb4f0a4b87a08a99e76d1abee3aedb3d995
Erik Hopmans <hopmans@stanford.edu>
2012-10-15 16:20:03 -0700

    Created output and logfile part

--- OSSEQ_coverageAverage.py
+++ OSSEQ_coverageAverage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Take the average of the cov.out files created by coverageBed
-#
+#Uses tabdelimited txt file with path in column 1 and filename in column 2
 #
 #  Created by Erik Hopmans on 10/12/12.
 #  Copyright (c) 2012 Ji Research Group - Stanford Genome Technology Center. All rights reserved.
@@ -18,18 +18,16 @@ import getpass
 ##### INPUTS AND OUTPUTS #####
 
 now = datetime.now()
-
+outputFile =  open(sys.argv[1][:-4]+"OUTPUT.txt", 'w')
+print >> outputFile, "Filename",'\t',"Average",'\t',"SD"
 
 #Logfile
-logFile = open(sys.argv[1][:-4]+".coverageAverageSDLOG.txt", 'w')
+logFile = open(sys.argv[1][:-4]+"LOG.txt", 'w')
 print >> logFile, "Script was run by user:", getpass.getuser()
 print >> logFile, "Name of the script:", __file__
 print >> logFile, "Date and time when script was started:", now.strftime("%Y-%m-%d %H:%M")
 print >> logFile, "Input file of this script:", sys.argv[1]
-print >> logFile, "Description of output data: coverage files (cov.out) and cov_by_region.txt"
-
-
-
+print >> logFile, "Description of output data: calculates average and stdev of coverage files (cov.out)"
 
 
 
@@ -55,5 +53,13 @@ for row in f:
 
     covOutFile = open(pathFile, 'r')
     coverage = np.loadtxt(covOutFile, skiprows=0, usecols=[4])
-    print np.average(coverage)
-    print np.std(coverage)
+    print >> outputFile, file,'\t',np.average(coverage),'\t',np.std(coverage)
+
+
+#cleanup and close files
+outputFile.close()
+
+#finishing logfile
+now = datetime.now()
+print >> logFile, "Date and time when script was finished:", now.strftime("%Y-%m-%d %H:%M")
+logFile.close()

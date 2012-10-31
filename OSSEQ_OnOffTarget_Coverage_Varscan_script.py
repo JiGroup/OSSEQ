e16895b4fa5fa70054217175f05626bcc005371e
Erik Hopmans <hopmans@stanford.edu>
2012-10-30 22:30:49 -0700

    Added coveragebed_script+replaced percalc

--- OSSEQ_OnOffTarget_Coverage_Varscan_script.py
+++ OSSEQ_OnOffTarget_Coverage_Varscan_script.py
@@ -23,7 +23,9 @@ print >> outputFile, "Filename",'\t',"Reads:",'\t',"Mapped Reads:",'\t',"NonMapp
 
 #### DEFINE FUNCTIONS #####
 
-def percentCalc(number, divisor):
+
+#Calculate percentage: if divisor is zero, an exception is required to prevent exciting in error
+def percentCalc(number, divisor):        
     if divisor > 0:
         percentage =  ((1.0*number)/divisor)*100
     else:
@@ -41,28 +43,19 @@ for row in f:
     fileBed450 = row[3]
     fileBed1500 = row[4]
     bam = pathBam+fileBam
-    bedFile450 = pathBed+fileBed450
-    bedFile1500 = pathBed+fileBed1500
+    bed450 = pathBed+fileBed450
+    bed1500 = pathBed+fileBed1500
        
     # Open bam files and bed files for processing
     inputFile = pysam.Samfile(bam, 'rb')
     bedtool = pybedtools.BedTool(bam)
-    bed450 = pybedtools.BedTool(bedFile450)
-    bed1500 = pybedtools.BedTool(bedFile1500)
+    bed450 = pybedtools.BedTool(bed450)
+    bed1500 = pybedtools.BedTool(bed1500)
     
     
-
-
-
-    
-    
-    #total nr of reads is mapped + unmapped (if zero, an exception is required for calculating the percAlignment to prevent error)
-    percAlignment = percentCalc(inputFile.mapped, inputFile.mapped+inputFile.unmapped)
-
     #nr of unmapped reads containing oligoC/D sequence (Georges used these strings for oligoC/D in previous analyses)
     oligoC = 0
     oligoD = 0
-
     for read in inputFile.fetch(until_eof=True):
         if read.is_unmapped:
             if 'CATTAAAAAA' in read.seq:
@@ -71,39 +64,47 @@ for row in f:
                 oligoD += 1
     
     
-    #On/Off target intersectbed (within 450bp or outside or 1500bp of capture probes (450bp region is searched within 1500bedtool (less sequences))
+    #On/Off target intersectbed (within 450bp or outside or 1500bp of capture probes (450bp region is searched within 1500bedtool (more efficient theoretically due to less sequences))
   
     bam_bed1500 = bedtool.intersect(bed1500)
     bam_bed1500Count = bam_bed1500.count()
-  
     bam_bed450 = bam_bed1500.intersect(bed450)
     bam_bed450Count = bam_bed450.count()
     
 
-    #On/Off target percentages
-    
-    if inputFile.mapped > 0:
-        onTargetPerc450 = (1.0*bam_bed450Count)/inputFile.mapped*100
-    else:
-        onTargetPerc450 = "NA"
-    
-    if inputFile.mapped > 0:
-        offTargetPerc1500 = (1.0*inputFile.mapped - bam_bed1500Count)/inputFile.mapped*100
-    else:
-        offTargetPerc1500 = "NA"
+    #Calculate percentages
+    percAlignment = percentCalc(inputFile.mapped, inputFile.mapped+inputFile.unmapped)
+    onTargetPerc450 = percentCalc(bam_bed450Count, inputFile.mapped)
+    offTargetPerc1500 = percentCalc(inputFile.mapped-bam_bed1500Count, inputFile.mapped)
+
     
     #####OUTPUT######## print to the output file
-    print >> outputFile, fileBam,'\t', inputFileTotalReads,'\t', inputFile.mapped,'\t', inputFile.unmapped,'\t', percAlignment,'\t', oligoC,'\t', oligoD,'\t', bam_bed450Count,'\t', onTargetPerc450,'\t', inputFile.mapped-bam_bed1500Count,'\t', offTargetPerc1500,'\t'
+    print >> outputFile, fileBam,'\t', inputFile.mapped+inputFile.unmapped,'\t', inputFile.mapped,'\t', inputFile.unmapped,'\t', percAlignment,'\t', oligoC,'\t', oligoD,'\t', bam_bed450Count,'\t', onTargetPerc450,'\t', inputFile.mapped-bam_bed1500Count,'\t', offTargetPerc1500,'\t'
+
+
+#This next part should typically only be for p1 files, not for p2 files.
+
+#Check if input files are correct, skip p2 files, check if input files are bam and bed files
+    if "p2." in fileBam:
+        continue
+
+#Do a coverage bed of the p1 bam files vs the bed450 files. -d option is for reporting the depth in each position present in the Bed file. Subsequently run John's total_up_cov_by_regions.pl script by loading the just created cov.out as an argument in the command.
+
+    coverageBed = "coverageBed -d -abam " +bam+ " -b " +bed450+ " > " +fileBam[:-4]+".cov.out"
+    os.system(coverageBed)
+
+#cov.out = bam.coverage(bed450).saveas(fileBam[:-4]+".cov.out")
+#
+    perlCovRegions = "perl total_up_cov_by_region.pl "+fileBam[:-4]+".cov.out"
+    os.system(perlCovRegions)
+
+
 
 
 #cleanup and close files
 outputFile.close()
 pybedtools.cleanup()
 
-#finishing logfile
-now = datetime.now()
-print >> logFile, "Date and time when script was finished:", now.strftime("%Y-%m-%d %H:%M")
-logFile.close()
 
 
 

7fdcd09c188eace2084f834ee04efa283850d416
Erik Hopmans <hopmans@stanford.edu>
2012-11-08 11:31:59 -0800

    implemented vlookup, iterating through files in directory

--- Varscan_Combine.py
+++ Varscan_Combine.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 # Varscan_combine.py takes all varscan files (based on ".out" extension) in the directory, combines the variants in each file into 1 combined table
-#
-#
+# It creates 2 types of files for this: the final file (infile.csv
+###TODO: explean more clearly
+##TODO: rename filenames to make them better understandable
 #  Created by Erik Hopmans on 11/6/12.
 #  Copyright (c) 2012 Ji Research Group - Stanford Genome Technology Center. All rights reserved.
 
@@ -14,111 +15,97 @@ import os
 #########################################             PART 1                ############################################################
 ##### INPUTS AND OUTPUTS #####
 
-outputFile = open("temp", "w")
+
+#Creating the infile Header:
 headers = [x for x in os.listdir('.') if x.endswith('.out')] #List the files ending with '.out' in the directory
-print >> outputFile, "Chrom_position",'\t',"Refbase",'t','\t'.join(headers) #Create Header (it would probably be better to add this after the sort unique step with something like http://stackoverflow.com/questions/3162314/add-headers-to-a-file )
+outputfile = open('infile.csv', 'wb')
+outwriter =  csv.writer(outputfile, delimiter=',')
+outwriter.writerow(['Chrom_position'] + ['RefBase']+headers)
 
 
 
 ##### SCRIPT #####
-#Creates a list of all the first 3 columns of the varscan files (chrom, position (combined to 1: chrom_position) and reference base), subsequently sort this list and retain the unique variant positions (by using shell sort and uniq commands)
+
+#Iterate through the snp.out files: create the infile.csv that has all chrompos in there and create the outtemp.csv files for each snp.out file to use as a dictionary in part2
 
 
 for fname in os.listdir('.'):
-    if fname.endswith('out'):
-        outTemp = open(fname+".outtemp.txt", 'w')
-        print >> outTemp, "ChromPosition",'\t',"VarBase"
-        with open(fname) as f:
+    if fname.endswith('.out'):
+        checkfile = open(fname+'.outtemp.csv', 'wb')        #Create Header for checkfiles
+        checkwriter =  csv.writer(checkfile, delimiter=',')
+        checkwriter.writerow(['Chrompos'] + ['varBase'])
+
+        with open(fname) as f:                                 #iterate through snp.out files
             for line in f:
                 if line.startswith("Chrom"):
                     continue
                 tab = line.split('\t')
                 pos = tab[0]+"_"+tab[1]
-                print >> outputFile, pos,'\t',tab[2]
-                print >> outTemp, pos,'\t',tab[2]
-        outTemp.close()
+                refBase = tab[2]
+                varBase = tab[3]
+                checkwriter.writerow([pos] + [varBase])        #add chrompos+varbase to snp.out.outtemp.csv file
+                outwriter.writerow([pos] + [refBase])           #Add chrom pos + refbase to infile
+        checkfile.close()
+outputfile.close()
 
-outputFile.close()
-
-sortUnique = "cat temp | sort -n | uniq > temp2.txt"
+#Sort and keep unique locations with shell code
+sortUnique = "cat infile.csv | sort -n | uniq > infile2.csv"
 os.system(sortUnique)
-
-
-#####################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Cleanup
-os.remove("temp")
-
-
-
-
-
-
 
 
 
 
 
 #########################################             PART 2                ############################################################
-#This part is basically a similar function as excels vlookup, it creates a dict(ionary) in which it searches for a specific value (comparable to the table in which excels vlookup searches)
+#This part is basically a similar function as excels vlookup, it creates a dict(ionary) named "checkfile" in which it searches for a specific value (comparable to the table in which excels vlookup searches)
 
 ##### INPUTS AND OUTPUTS #####
-#infile = "temp2.txt"
-#
-#for fname in os.listdir('.'):
-#    if fname.endswith('.outtemp.txt'):
-#        outputColumn = fname
-#
+infile2 = "infile2.csv"
 
-fname = "20120309_SG1_0121.c1_p1.good.stontarget.snp.out.outtemp.txt"
-outputColumn = fname
-# Open the check file in a context manager. This ensures the file will be closed
-# correctly if an error occurs.EH: Adapted from http://codereview.stackexchange.com/questions/7113/searching-a-value-from-one-csv-file-in-another-csv-file-python
-with open('checkfile_tab2.txt', 'rU') as checkfile:                          # It is important to use rU = universal read mode, prevents errors due to delimeters
-    checkreader = csv.DictReader(checkfile, delimiter='\t')
-    
-    
-    # This does the real work. The middle line is a generator expression which
-    # iterates over each line in the check file. The base code and stock
-    # level are extracted from each line. This is then converted
-    # into a dictionary. This dictionary has the base codes as its keys and
-    # their result code as its values.
-    varBase = dict(
-                          (v['ChromPos'], v['VarBase']) for v in checkreader
-                          )
+for fname in os.listdir('.'):
+    if fname.endswith('.outtemp.csv'):
+        outputColumn = fname[:-12]
 
-# Open the input and output files.
-with open("temp2.txt", 'rU') as infile:
-    with open('outfile.csv', 'wb') as outfile:
-        reader = csv.DictReader(infile, delimiter='\t')
-        # Use the same field names for the output file.
-        writer = csv.DictWriter(outfile, reader.fieldnames)
-        #writer = csv.DictWriter(outfile, fieldnames=('StockNumber', 'SKU', 'ChannelProfileID'), delimiter=',')
-        writer.writeheader()
-        
-        # Iterate over the bases in the input.
-        for base in reader:
-            # Find the stock level from the dictionary we created earlier. Using
-            # the get() method allows us to specify a default value if the SKU
-            # does not exist in the dictionary.
-            result = varBase.get(base['Chrom_position'], "-")
-            
-            # Update the base info.
-            base[outputColumn] = result
-            
-            # Write it to the output file.
-            writer.writerow(base)
+	# Open the check file in a context manager. This ensures the file will be closed
+	# correctly if an error occurs.EH: Adapted from http://codereview.stackexchange.com/questions/7113/searching-a-value-from-one-csv-file-in-another-csv-file-python
+	with open(fname, 'rb') as checkfile:                          
+		checkreader = csv.DictReader(checkfile)
+		
+		
+		# This does the real work. The middle line is a generator expression which
+		# iterates over each line in the check file. The base code and stock
+		# level are extracted from each line. This is then converted
+		# into a dictionary. This dictionary has the base codes as its keys and
+		# their result code as its values.
+		varBase = dict(
+							  (v['Chrompos'], v['varBase']) for v in checkreader
+							  )
+	
+	# Open the input and output files.
+	with open(infile2, 'rb') as infile:
+		with open('outfile.csv', 'wb') as outfile:
+			reader = csv.DictReader(infile)
+			# Use the same field names for the output file.
+			writer = csv.DictWriter(outfile, reader.fieldnames)
+			#writer = csv.DictWriter(outfile, fieldnames=('StockNumber', 'SKU', 'ChannelProfileID'), delimiter=',')
+			writer.writeheader()
+			
+			# Iterate over the bases in the input.
+			for base in reader:
+				# Find the stock level from the dictionary we created earlier. Using
+				# the get() method allows us to specify a default value if the SKU
+				# does not exist in the dictionary.
+				result = varBase.get(base['Chrom_position'], " ")
+				
+				# Update the base info.
+				base[outputColumn] = result
+				
+				# Write it to the output file.
+				writer.writerow(base)
+	os.rename("outfile.csv", "infile2.csv")	
+
+
+
+
+#Cleanup
+os.remove("infile.csv"+)

2fb2153ef55f70f13d3abe297ebce0c640bc776f
Erik Hopmans <hopmans@stanford.edu>
2012-11-08 12:02:48 -0800

    renamed variables for clarification, comments, improved cleanup

--- Varscan_Combine.py
+++ Varscan_Combine.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
-# Varscan_combine.py takes all varscan files (based on ".out" extension) in the directory, combines the variants in each file into 1 combined table
-# It creates 2 types of files for this: the final file (infile.csv
-###TODO: explean more clearly
-##TODO: rename filenames to make them better understandable
+# Varscan_combine.py takes all varscan files (based on ".out" extension) in the directory, combines the variants from each file into 1 combined table
+# It creates 2 types of files for this: the final file (temp.csv
+
 #  Created by Erik Hopmans on 11/6/12.
 #  Copyright (c) 2012 Ji Research Group - Stanford Genome Technology Center. All rights reserved.
 
@@ -11,14 +10,16 @@
 import csv
 import sys
 import os
+from datetime import datetime
 
 #########################################             PART 1                ############################################################
 ##### INPUTS AND OUTPUTS #####
+now = datetime.now()		#current date and time for outputfile
 
+#Creating the outputfile Header (named temp.csv here and Varcan_Combine_output_date further on).
 
-#Creating the infile Header:
 headers = [x for x in os.listdir('.') if x.endswith('.out')] #List the files ending with '.out' in the directory
-outputfile = open('infile.csv', 'wb')
+outputfile = open('temp.csv', 'wb')
 outwriter =  csv.writer(outputfile, delimiter=',')
 outwriter.writerow(['Chrom_position'] + ['RefBase']+headers)
 
@@ -26,7 +27,9 @@ outwriter.writerow(['Chrom_position'] + ['RefBase']+headers)
 
 ##### SCRIPT #####
 
-#Iterate through the snp.out files: create the infile.csv that has all chrompos in there and create the outtemp.csv files for each snp.out file to use as a dictionary in part2
+#Iterate through the snp.out files, 2 files are created in this step:
+#1. Checkfiles: A file for each snp.out file (outtemp.csv) with the combined column for chromosome and position (X_xxxxxxxx format) in csv format (needed csv to get PART 2 to work)
+#2. outputfile: Add each chrompos and reference base to the temp.csv outputfile, this will be sorted and unique chrompos will be kept, renamed to Varcan_Combine_output_date.
 
 
 for fname in os.listdir('.'):
@@ -37,19 +40,19 @@ for fname in os.listdir('.'):
 
         with open(fname) as f:                                 #iterate through snp.out files
             for line in f:
-                if line.startswith("Chrom"):
+                if line.startswith("Chrom"):				#skip header
                     continue
                 tab = line.split('\t')
-                pos = tab[0]+"_"+tab[1]
+                pos = tab[0]+"_"+tab[1]						#create Chrom_position
                 refBase = tab[2]
                 varBase = tab[3]
-                checkwriter.writerow([pos] + [varBase])        #add chrompos+varbase to snp.out.outtemp.csv file
-                outwriter.writerow([pos] + [refBase])           #Add chrom pos + refbase to infile
+                checkwriter.writerow([pos] + [varBase])        #add chrompos+varbase to checkfile
+                outwriter.writerow([pos] + [refBase])           #Add chrom pos + refbase to outputfile
         checkfile.close()
 outputfile.close()
 
-#Sort and keep unique locations with shell code
-sortUnique = "cat infile.csv | sort -n | uniq > infile2.csv"
+#Sort and keep unique locations with shell code (works quick)
+sortUnique = "cat temp.csv | sort -n | uniq > Varscan_Combine_Output_"+now.strftime("%Y%m%d_%H%M")+".csv"
 os.system(sortUnique)
 
 
@@ -57,23 +60,24 @@ os.system(sortUnique)
 
 
 #########################################             PART 2                ############################################################
-#This part is basically a similar function as excels vlookup, it creates a dict(ionary) named "checkfile" in which it searches for a specific value (comparable to the table in which excels vlookup searches)
+#This part is basically a similar function as excels vlookup, it creates a dict(ionary) from a "checkfile" in which it searches for a specific value (comparable to the table in which excels vlookup searches)
+#Copied and adapted from http://codereview.stackexchange.com/questions/7113/searching-a-value-from-one-csv-file-in-another-csv-file-python
 
 ##### INPUTS AND OUTPUTS #####
-infile2 = "infile2.csv"
+finalFile = "Varscan_Combine_Output_"+now.strftime("%Y%m%d_%H%M")+".csv"
 
 for fname in os.listdir('.'):
     if fname.endswith('.outtemp.csv'):
         outputColumn = fname[:-12]
 
 	# Open the check file in a context manager. This ensures the file will be closed
-	# correctly if an error occurs.EH: Adapted from http://codereview.stackexchange.com/questions/7113/searching-a-value-from-one-csv-file-in-another-csv-file-python
+	# correctly if an error occurs.
 	with open(fname, 'rb') as checkfile:                          
 		checkreader = csv.DictReader(checkfile)
 		
 		
 		# This does the real work. The middle line is a generator expression which
-		# iterates over each line in the check file. The base code and stock
+		# iterates over each line in the check file. The Chrompos and varBase
 		# level are extracted from each line. This is then converted
 		# into a dictionary. This dictionary has the base codes as its keys and
 		# their result code as its values.
@@ -82,7 +86,7 @@ for fname in os.listdir('.'):
 							  )
 	
 	# Open the input and output files.
-	with open(infile2, 'rb') as infile:
+	with open(finalFile, 'rb') as infile:
 		with open('outfile.csv', 'wb') as outfile:
 			reader = csv.DictReader(infile)
 			# Use the same field names for the output file.
@@ -102,10 +106,12 @@ for fname in os.listdir('.'):
 				
 				# Write it to the output file.
 				writer.writerow(base)
-	os.rename("outfile.csv", "infile2.csv")	
+	os.rename("outfile.csv", "Varscan_Combine_Output_"+now.strftime("%Y%m%d_%H%M")+".csv")	
 
 
 
 
 #Cleanup
-os.remove("infile.csv"+)
+for fname in os.listdir('.'):
+    if fname.endswith('temp.csv'):
+    	os.remove(fname)
